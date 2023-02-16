243a8f30118a53e0cb2f84ceefc1aaf1a7bcc504
rimMicrosoft <methni.rim@gmail.com>
2023-02-16 16:51:43 +0100

    change url environement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api/',
+  apiUrl: 'https://savsonom-api.herokuapp.com/api/',
   apiRoutes: {
     userLists: 'fiches',
     addUser:'fiche',
