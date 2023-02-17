a0495a9a582088654eb7dd49579beffd2c1d7fb4
rimMicrosoft <methni.rim@gmail.com>
2023-02-17 12:55:26 +0100

    RECAP

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/savpwa'));
+app.use(express.static(__dirname + '/dist/join-eight'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/join-eight/index.html'));});
